d88d9a1615e9f3bf39ca9082a94cf582b8da5bbb
Yusuke KON <yusukedrive@gmail.com>
2016-11-19 11:07:36 +0900

    Comment out sample shell

--- sh/backup.sh
+++ sh/backup.sh
@@ -1,17 +1,17 @@
 # sample backup shell
-echo "start daily buckup!!"
+# echo "start daily buckup!!"
 
-echo "== stopping worker =="
-heroku ps:scale worker=0 -a APP_BE_BAKED_UP
+# echo "== stopping worker =="
+# heroku ps:scale worker=0 -a APP_BE_BAKED_UP
 
-echo "== copying database to preview from production =="
-heroku pg:copy ORIGINAL_DB_NAME DB_TO_BE_COPIED --confirm APP_BE_BAKED_UP -a APP_BE_BAKED_UP
+# echo "== copying database to preview from production =="
+# heroku pg:copy ORIGINAL_DB_NAME DB_TO_BE_COPIED --confirm APP_BE_BAKED_UP -a APP_BE_BAKED_UP
 
-echo "== clearing jobs =="
-heroku run rake jobs:clear -a APP_BE_BAKED_UP
+# echo "== clearing jobs =="
+# heroku run rake jobs:clear -a APP_BE_BAKED_UP
 
-echo "== copying s3 bucket to staging from production =="
-aws s3 sync --delete --acl public-read s3://ORIGINAL_BUCKET s3://BUCKET_TO_BE_COPIED
+# echo "== copying s3 bucket to staging from production =="
+# aws s3 sync --delete --acl public-read s3://ORIGINAL_BUCKET s3://BUCKET_TO_BE_COPIED
 
-echo "== starting worker =="
-heroku ps:scale worker=1 -a APP_BE_BAKED_UP
+# echo "== starting worker =="
+# heroku ps:scale worker=1 -a APP_BE_BAKED_UP
